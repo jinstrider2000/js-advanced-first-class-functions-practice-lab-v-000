fa002694302b51cb982fecd97e54ba730533a0be
Efrain Perez Jr <efrainperezjr@live.com>
2018-04-14 13:42:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 // Code your solution in this file!
-const logDriverNames = (drivers) => {
+function logDriverNames(drivers) {
   drivers.forEach((driver) => console.log(driver.name);)
 }
 
 function logDriversByHometown() {
-
+  
 }
 
 function driversByRevenue() {
-
+  
 }
 
 function driversByName() {
-
+  
 }
 
 function totalRevenue() {
-
+  
 }
 
 function averageRevenue() {
-
+  
 }
